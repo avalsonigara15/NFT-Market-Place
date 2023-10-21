b8079fa509f94f8e511312ffa6a85f883e0a9b47
avalsonigara15 <avals1504@gmail.com>
2023-10-21 14:26:06 +0530

    update

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import Profile from "./components/Profile";
 import SellNFT from "./components/SellNFT";
 import NFTPage from "./components/NFTpage";
 
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="container">
       <Routes>
         <Route path="/" element={<Marketplace />} />
-        <Route path="/nftPage" element={<NFTPage />} />
+        <Route path="/nftPage/:tokenId" element={<NFTPage />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/sellNFT" element={<SellNFT />} />
       </Routes>
